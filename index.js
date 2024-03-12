7731b651e48a488917e70b4bdfd4862d6651e652
neandercode <josephjones555@yahoo.com>
2024-03-11 21:16:50 -0400

    random thought I had for an object

---
+++ index.js
@@ -0,0 +1,16 @@
+let person = {
+  name: "Pendejo",
+  age: 35,
+  occupation: "software developer",
+  hometown: "Awesome, Texas",
+  "likes tacos": true,
+};
+
+alert(
+  "Hello, my name is " +
+    person.name +
+    ". I am " +
+    person.age +
+    " and I am from " +
+    person.hometown
+);
